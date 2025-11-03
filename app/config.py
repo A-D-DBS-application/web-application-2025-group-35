16554025c0b438cf11521e774cb7c5c171ab33be
KlaasDhaen <klaas.dhaen@ugent.be>
2025-11-03 10:38:14 +0100

    wachtwoord aangepast

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ import secrets
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[YOUR_PASSWORD]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[OpWielekes123]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
