550bd191f412672f68ad6ee945670cae44eab1bf
KlaasDhaen <klaas.dhaen@ugent.be>
2025-11-03 10:14:38 +0100

    Update config.py

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[YOUR_PASSWORD]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
