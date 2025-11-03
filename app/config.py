f66b617ea9788c333cbfdd1de0a8404cb123d765
KlaasDhaen <klaas.dhaen@ugent.be>
2025-11-03 10:18:53 +0100

    secret key in orde

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
+import os
+import secrets
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[YOUR_PASSWORD]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
