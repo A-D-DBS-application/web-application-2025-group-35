930123dcab8c1259dabda6fd4966094c2b707f85
dag <dag.pieters@ugent.be>
2025-11-06 22:51:51 +0100

    test van init en run

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[OpWielekes123]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+#postgresql://postgres.iujvntadipxpwozzacty:OpWielekes123@aws-1-eu-central-1.pooler.supabase.com:5432/postgres
+#kheb de code al eens laten runnen om te zien of het compiled, supabase wou niet connecteren vr mij maar met de link hierboven wel
+#miss kunnen we eens testen of het bij jullie ook het geval is
