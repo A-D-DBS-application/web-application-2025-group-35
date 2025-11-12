413d004ff9865d1d705b5c084d596ed8639ae646
dag <dag.pieters@ugent.be>
2025-11-12 14:14:22 +0100

    deleted create all

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .models import db #maakt het mogelijk om database te koppelen aan flask app
 from .config import Config #zorgt ervoor dat de app weet waar de database staat
+from flask_migrate import Migrate
 
 def create_app():
     app=Flask(__name__)#Hier maak je de Flask app-instantie aan.
@@ -10,8 +11,8 @@ def create_app():
 
     db.init_app(app) #koppel SQLalchemy database aan flask app
 
-    with app.app_context(): 
-        db.create_all() #activeert app context zodat flask weet welke app actief is, dan maakt alle tabellen van de database aan
+    
+    migrate.init_app(app, db) #activeert app context zodat flask weet welke app actief is, dan maakt alle tabellen van de database aan
     
     from .routes import main  #Hier importeer je je blueprint (main), die je routes (endpoints) bevat.
     app.register_blueprint(main) #Je “registreert” de blueprint bij de Flask app, zodat de routes actief worden.
