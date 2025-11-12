702f85696046290c82450ee22f4d413e4138fb1f
SophieRousere <sophie.rousere@gmail.com>
2025-11-12 17:48:18 +0100

    routes en templates

---
+++ app/routes.py
@@ -0,0 +1,35 @@
+#routes om data uit je modellen te halen en naar templates te sturen
+from flask import Blueprint, render_template
+from .models import db, Adres, Verantwoordelijke, Kind, Item, Betaling, Magazijn, Medewerker
+
+main = Blueprint('main', __name__)  #blueprint main gebruiken zodat alles net georganiseerd is
+
+#startpagina
+@main.route('/')
+def index():
+    return render_template('index.html') 
+
+# Pagina Klanten
+@main.route('/klanten')  #route die '/klanten' URL afhandelt.
+def klanten():
+    verantwoordelijken = Verantwoordelijke.query.all()   #haalt alle verantwoordelijken uit de database
+    return render_template('klanten.html', verantwoordelijken=verantwoordelijken) #stuurt de data naar de template
+
+# Pagina Fietsen
+@main.route('/fietsen')
+def fietsen():
+    items = Item.query.all()
+    return render_template('fietsen.html', items=items)
+
+# Pagina Verhuur
+@main.route('/verhuur')
+def verhuur():
+    betalingen = Betaling.query.all()
+    return render_template('verhuur.html', betalingen=betalingen)
+
+# Pagina Financieel
+@main.route('/financieel')
+def financieel():
+    betalingen = Betaling.query.all()
+    return render_template('financieel.html', betalingen=betalingen)
+
