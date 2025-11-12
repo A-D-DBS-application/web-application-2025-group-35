1d7f07696460119e44d63abaee5be72bbd78523a
dag <dag.pieters@ugent.be>
2025-11-12 13:24:55 +0100

    fixen link

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ import secrets
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[OpWielekes123]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.iujvntadipxpwozzacty:OpWielekes123@aws-1-eu-central-1.pooler.supabase.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #postgresql://postgres.iujvntadipxpwozzacty:OpWielekes123@aws-1-eu-central-1.pooler.supabase.com:5432/postgres
-#kheb de code al eens laten runnen om te zien of het compiled, supabase wou niet connecteren vr mij maar met de link hierboven wel
-#miss kunnen we eens testen of het bij jullie ook het geval is
+#postgresql://postgres:[OpWielekes123]@db.iujvntadipxpwozzacty.supabase.co:5432/postgres
+#link is aangepast want het compiled anders niet
