fd9898a25d033ba6e5e6c1fbae7b0b3bfaba7755
KlaasDhaen <klaas.dhaen@ugent.be>
2025-11-24 16:03:28 +0100

    code die al deels runt

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,36 @@
-# routes om data uit je modellen te halen en naar templates te sturen
 from flask import Blueprint, render_template, request
 from .models import db, Adres, Verantwoordelijke, Kind, Item, Betaling, Magazijn, Medewerker
-from .algorithm import fietsen_voor_leeftijd  # ons algoritme importeren
+from .algorithm import fietsen_voor_leeftijd
 
-main = Blueprint('main', __name__)  # blueprint main gebruiken zodat alles net georganiseerd is
+main = Blueprint('main', __name__)
 
-# startpagina
 @main.route('/')
 def index():
-    return render_template('index.html')  # Hier renderen we gewoon index.html zoals het is
+    return render_template('home.html')
 
-# Pagina Klanten
 @main.route('/klanten')
 def klanten():
-    verantwoordelijken = Verantwoordelijke.query.all()  # haalt alle verantwoordelijken uit de database
+    verantwoordelijken = Verantwoordelijke.query.all()
     return render_template('klanten.html', verantwoordelijken=verantwoordelijken)
 
-# Pagina Fietsen
 @main.route('/fietsen')
 def fietsen():
-    leeftijd = request.args.get('leeftijd', type=int)  # haal ?leeftijd=12 op uit URL
+    leeftijd = request.args.get('leeftijd', type=int)
 
     if leeftijd is not None:
-        items = fietsen_voor_leeftijd(leeftijd)  # filter fietsen via algoritme
+        items = fietsen_voor_leeftijd(leeftijd)
     else:
-        items = Item.query.all()  # toon alles als geen leeftijd is opgegeven
+        items = Item.query.all()
 
     return render_template('fietsen.html', items=items, leeftijd=leeftijd)
 
-# Pagina Verhuur
 @main.route('/verhuur')
 def verhuur():
     betalingen = Betaling.query.all()
     return render_template('verhuur.html', betalingen=betalingen)
 
-# Pagina Financieel
 @main.route('/financieel')
 def financieel():
     betalingen = Betaling.query.all()
     return render_template('financieel.html', betalingen=betalingen)
+
